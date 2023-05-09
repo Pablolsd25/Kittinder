ff7f14e33e080444350fedb40aa66618b923d715
Pablolsd2501~ <pablot_comercial.alamo@hotmail.com>
2023-05-08 23:11:07 -0600

    Inicio de aplicacion renderizando imagen

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import './App.css'
+
+function App() {
+  const [catImage, setCatImage] = useState('')
+  useEffect(()=>{
+    fetch('https://api.thecatapi.com/v1/images/search')
+    .then(res => res.json())
+    .then(data =>setCatImage(data[0].url))
+    .catch(error=>console.error(error));
+  
+  },[]);
+
+  return (
+    <>
+    <h1>Renderizando imagenes de gatos</h1>
+    <img src={catImage} alt="img-cat"/>
+    </>
+  )
+}
+
+export default App
