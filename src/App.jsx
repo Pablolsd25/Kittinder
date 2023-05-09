0bb1088f8b2aaa4d15b2d2b928432abae3b2bffd
Pablolsd2501~ <pablot_comercial.alamo@hotmail.com>
2023-05-08 23:35:33 -0600

    Se agrego boton

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,35 @@
 import React, { useState, useEffect } from 'react';
-import './App.css'
 
 function App() {
-  const [catImage, setCatImage] = useState('')
-  useEffect(()=>{
-    fetch('https://api.thecatapi.com/v1/images/search')
-    .then(res => res.json())
-    .then(data =>setCatImage(data[0].url))
-    .catch(error=>console.error(error));
-  
-  },[]);
+  const [catImage, setCatImage] = useState('');
+  const [isFetching, setIsFetching] = useState(false);
+
+  const handleNewCatClick = () => {
+    setIsFetching(true);
+  };
+
+  useEffect(() => {
+    if (isFetching) {
+      fetch('https://api.thecatapi.com/v1/images/search')
+        .then(response => response.json())
+        .then(data => {
+          setCatImage(data[0].url);
+          setIsFetching(false);
+        })
+        .catch(error => console.error(error));
+    }
+  }, [isFetching]);
 
   return (
-    
-    <>
-    <h1>Renderizando imagenes de gatos</h1>
-    <img src={catImage} alt="img-cat"/>
-    </>
-  )
+    <div>
+      <h1>Imágenes de gatos</h1>
+      <img src={catImage} alt="Imagen de gato" />
+      <br />
+      <button onClick={handleNewCatClick} disabled={isFetching}>
+        {isFetching ? 'Cargando...' : 'Nuevo gato'}
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
